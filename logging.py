1932387d56a1e1443477aa9cb2820661716fa311
Alex <yakimchuk.nstu@gmail.com>
2020-05-28 14:23:28 +0200

    Create logging file
    Set up this for Django logging you will get all SQL queries in your logs

---
+++ logging.py
@@ -0,0 +1,18 @@
+#logging all sql queryes
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
